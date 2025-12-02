c31b35188ffbda766de2495cfff861d8de9eb689
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-12-02 23:41:55 +0530

    Implement insertion sort algorithm in Python

---
+++ Insertion-Sorting.py
@@ -0,0 +1,11 @@
+class Solution:
+    def insertionSort(self, nums):
+        for i in range(1,len(nums)):
+            j=i 
+            while (j>0 and  nums[j-1]>nums[j]):
+                nums[j-1],nums[j]= nums[j],nums[j-1]
+                j-=1
+        return nums
+n=input().strip("[]")
+n=list(map(int,n.split(",")))
+print(Solution().insertionSort(n))
